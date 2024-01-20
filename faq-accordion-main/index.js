fd8b3eecd57a10d434333f64b9f56b6fae78de02
Tini369 <156963175+Tini369@users.noreply.github.com>
2024-01-20 19:28:18 +0100

    Add files via upload

---
+++ faq-accordion-main/index.js
@@ -0,0 +1,18 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+function myFunction1() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+
